916d761f5c0d934ee694a5191f58b332127f0d15
imadkhefifdev <imadkhefifdev@gmail.com>
2026-03-23 09:24:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,22 @@
-
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 import requests
 
-TOKEN = "8684287596:AAFkCfJgL2oirY6QbH67baISNjHAf8GjgGQ"
+TOKEN = os.getenv("TOKEN")
 
-# جلب السعر من API
 def get_rate(from_currency, to_currency):
     url = f"https://api.exchangerate-api.com/v4/latest/{from_currency.upper()}"
     response = requests.get(url)
     data = response.json()
     return data["rates"].get(to_currency.upper())
 
-# معالجة الرسائل
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text.lower()
 
     try:
         parts = text.split()
 
-        # مثال: 100 usd to dzd
         if len(parts) == 4 and parts[2] == "to":
             amount = float(parts[0])
             from_currency = parts[1]
@@ -38,12 +35,11 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         else:
             await update.message.reply_text("📌 اكتب: 100 usd to dzd")
 
-    except:
+    except Exception as e:
+        print(e)
         await update.message.reply_text("❌ خطأ في الإدخال")
 
-# تشغيل البوت
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 print("Bot is running...")
